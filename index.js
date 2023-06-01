bc7940476d0d4bd6cc8b29ca52061dc031f57db9
Mateusz Szczech <matle0410@gmail.com>
2023-06-01 17:06:42 +0200

    Add basic REST API for shows and episodes

--- index.js
+++ index.js
@@ -7,14 +7,63 @@ const port = 3000;
 const prisma = new PrismaClient();
 
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// BigInt toJSON function inplementation
+// eslint-disable-next-line no-extend-native, func-names
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
 
 app.get('/shows', async (req, res) => {
   const shows = await prisma.show.findMany();
   res.json(shows);
+});
+
+app.get('/shows/:id', async (req, res) => {
+  const show = await prisma.show.findMany({
+    where: { id: Number(req.params.id) },
+    include: {
+      episodes: true,
+    },
+  });
+
+  res.json(show);
+});
+
+app.post('/shows', async (req, res) => {
+  const {
+    name, description, country, imagePath,
+  } = req.body;
+
+  const result = await prisma.show.create({
+    data: {
+      name,
+      description,
+      country,
+      imagePath,
+    },
+  });
+
+  res.json(result);
+});
+
+app.post('/episodes', async (req, res) => {
+  const {
+    number, season, name, airDate, showName,
+  } = req.body;
+
+  const result = await prisma.episode.create({
+    data: {
+      number,
+      season,
+      name,
+      airDate: new Date(airDate),
+      show: { connect: { name: showName } },
+    },
+  });
+
+  res.json(result);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
