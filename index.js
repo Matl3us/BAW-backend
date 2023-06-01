caba927ffee5ac3975e8460f2adc12df447e2a5f
Mateusz Szczech <matle0410@gmail.com>
2023-06-01 16:11:08 +0200

    Setup basic packages

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require("express");
+const { PrismaClient } = require('@prisma/client');
+const express = require('express');
 const cors = require('cors');
 
 const app = express();
 const port = 3000;
+const prisma = new PrismaClient();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-app.listen(port, () =>
-  console.log(`App listening on port ${port}!`)
-);
+app.get('/shows', async (req, res) => {
+  const shows = await prisma.show.findMany();
+  res.json(shows);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
