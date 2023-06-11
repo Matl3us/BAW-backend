93b0eb82d94692f6432571d59fc4b6113a99aec0
Mateusz Szczęch <63805531+Matl3us@users.noreply.github.com>
2023-06-11 11:12:01 +0200

    Add main page and 404 error page

--- index.js
+++ index.js
@@ -20,13 +20,21 @@ BigInt.prototype.toJSON = function () {
 
 app.use(cors());
 app.use(express.json());
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+app.get('/', (req, res) => {
+  res.send('Backend API for creating show watching list');
+});
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/shows', showsRouter);
 app.use('/api/episodes', episodesRouter);
 app.use('/api/lists', listsRouter);
 app.use('/api/ratings', ratingsRouter);
+
+app.use((req, res) => {
+  res.status(404).send('not found');
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
