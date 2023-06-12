575385f52fb91c0369be4cf5f53621907bad7ed9
Mateusz Szczęch <63805531+Matl3us@users.noreply.github.com>
2023-06-12 12:46:20 +0200

    Pagination fix

--- routes/shows.js
+++ routes/shows.js
@@ -11,7 +11,7 @@ showsRouter.get('/', async (req, res) => {
 
   const result = {};
 
-  result.nextPage = shows.length < 20 ? null : `/shows?page=${+page + 1}`;
+  result.nextPage = shows.length <= 20 ? null : `/shows?page=${+page + 1}`;
   result.prevPage = page > 1 ? `/shows?page=${+page - 1}` : null;
 
   result.shows = JSON.parse(JSON.stringify(shows));
