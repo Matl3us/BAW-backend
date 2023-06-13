025104b16149afb705dd150982059746ae2b776e
Mateusz Szczęch <63805531+Matl3us@users.noreply.github.com>
2023-06-13 18:42:34 +0200

    Add endpoints descriptions in swagger docs

--- utils/swagger.js
+++ utils/swagger.js
@@ -21,25 +21,59 @@ const swaggerDefinition = {
     { name: 'User' },
   ],
   paths: {
-    '/api/episodes': { post: { tags: ['Episode'] } },
+    '/api/episodes': {
+      post: { tags: ['Episode'], summary: 'Add a new episode' },
+    },
+
     '/api/lists': {
-      post: { tags: ['List'] },
-      get: { tags: ['List'] },
-      delete: { tags: ['List'] },
+      post: { tags: ['List'], summary: 'Add a new tv show to the watch list' },
+      get: {
+        tags: ['List'],
+        summary: "Get a list of tv shows from user's watchlist",
+      },
+      delete: {
+        tags: ['List'],
+        summary: "Delete tv show from user's watchlist",
+      },
     },
-    '/api/ratings': { post: { tags: ['Rating'] } },
-    '/api/ratings/user': { get: { tags: ['Rating'] } },
+
+    '/api/ratings': {
+      post: { tags: ['Rating'], summary: 'Add a new rating to the tv show' },
+    },
+    '/api/ratings/user': {
+      get: {
+        tags: ['Rating'],
+        summary: "Get a list of user's tv show ratings",
+      },
+    },
     '/api/ratings/{id}': {
-      put: { tags: ['Rating'] },
-      delete: { tags: ['Rating'] },
+      put: { tags: ['Rating'], summary: 'Update tv show rating' },
+      delete: { tags: ['Rating'], summary: 'Delete tv show rating' },
     },
-    '/api/shows': { get: { tags: ['Show'] }, post: { tags: ['Show'] } },
-    '/api/shows/all': { get: { tags: ['Show'] } },
-    '/api/shows/{id}': { get: { tags: ['Show'] } },
-    '/api/login': { post: { tags: ['User'] } },
-    '/api/users/register': { post: { tags: ['User'] } },
-    '/api/shows/profile': { get: { tags: ['User'] } },
-    '/api/shows/password': { put: { tags: ['User'] } },
+
+    '/api/shows': {
+      get: { tags: ['Show'], summary: 'Get a paginated list of tv shows' },
+      post: { tags: ['Show'], summary: 'Add a new tv show' },
+    },
+    '/api/shows/all': {
+      get: { tags: ['Show'], summary: 'Get all tv shows with search query' },
+    },
+    '/api/shows/{id}': {
+      get: { tags: ['Show'], summary: 'Get a details of a tv show' },
+    },
+
+    '/api/login': {
+      post: { tags: ['User'], summary: 'User login' },
+    },
+    '/api/users/register': {
+      post: { tags: ['User'], summary: 'Register a new user' },
+    },
+    '/api/users/password': {
+      put: { tags: ['User'], summary: 'Change user\'s password' },
+    },
+    '/api/users/profile': {
+      get: { tags: ['User'], summary: 'Get user\'s detailed information' },
+    },
   },
 };
 
