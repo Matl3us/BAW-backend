4710712247db420c973e6fc0371415951c556e3a
Mateusz Szczęch <63805531+Matl3us@users.noreply.github.com>
2023-06-13 18:27:20 +0200

    Add swagger documentation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BAW-backend
+
+Swagger documentation [/docs](localhost:3000/docs)
--- utils/swagger.js
+++ utils/swagger.js
@@ -13,6 +13,34 @@ const swaggerDefinition = {
       description: 'Development server',
     },
   ],
+  tags: [
+    { name: 'Episode' },
+    { name: 'List' },
+    { name: 'Rating' },
+    { name: 'Show' },
+    { name: 'User' },
+  ],
+  paths: {
+    '/api/episodes': { post: { tags: ['Episode'] } },
+    '/api/lists': {
+      post: { tags: ['List'] },
+      get: { tags: ['List'] },
+      delete: { tags: ['List'] },
+    },
+    '/api/ratings': { post: { tags: ['Rating'] } },
+    '/api/ratings/user': { get: { tags: ['Rating'] } },
+    '/api/ratings/{id}': {
+      put: { tags: ['Rating'] },
+      delete: { tags: ['Rating'] },
+    },
+    '/api/shows': { get: { tags: ['Show'] }, post: { tags: ['Show'] } },
+    '/api/shows/all': { get: { tags: ['Show'] } },
+    '/api/shows/{id}': { get: { tags: ['Show'] } },
+    '/api/login': { post: { tags: ['User'] } },
+    '/api/users/register': { post: { tags: ['User'] } },
+    '/api/shows/profile': { get: { tags: ['User'] } },
+    '/api/shows/password': { put: { tags: ['User'] } },
+  },
 };
 
 const options = {
